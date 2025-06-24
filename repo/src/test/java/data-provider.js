eb8ab4d6d0c2712f226858ecb0d8cf5ed59fd75a
joshuaPerezpe <joshuaperez_2@hotmail.com>
2025-06-23 21:25:19 -0400

    cammbios

--- repo/src/test/java/data-provider.js
+++
@@ -1,8 +0,0 @@
-function fn(idProject) {
-    const DataProviders = Java.type('integration.helpers.DP.DataProviders');
-    const getDataProver = DataProviders.getFromDP(idProject);
-    if(getDataProver!=null){
-        return { data: JSON.parse(getDataProver) };
-    }
-    return null
-}
